45df1569f4eda7ffe5ffcd0717c0ed08df10c9e1
Ram4GB <leminhcuong2988@yahoo.com>
2022-08-06 18:02:39 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <el-form :model="form" ref="formRef">
+      <el-form-item 
+        :rules="rules.username" 
+        label="Username"
+        prop="username"
+        :required="true"
+      >
+        <el-input 
+          placeholder="Username"
+          v-model="form.username"
+        />
+      </el-form-item>
+      <el-form-item 
+        :rules="rules.password" 
+        label="Password"
+        prop="password"
+        :required="true"
+      >
+        <el-input 
+          placeholder="Password" 
+          v-model="form.password"
+        />
+      </el-form-item>
+      <el-form-item
+        :rules="rules.sick" 
+        prop="sick"
+        label="Are you sick in a few day ago?"
+      >
+        <el-radio-group @change="handleChangeRadio" v-model="form.sick">
+          <el-radio label="No I am not"></el-radio>
+          <el-radio label="Yes I am"></el-radio>
+          <el-radio label="I do not remember"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-button @click="handleSubmit" type="primary">Submit</el-button>
+    </el-form>
+    <p>loginForm: {{ loginForm }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+        sick: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: 'Please enter username', trigger: ['change', 'blur'] },
+          {
+            validator(rule, value, callback) {
+              console.log('validator rule, value, callback username');
+
+              if(value === 'leminhcuong') {
+                return callback('Please enter another username')
+              }
+
+              return callback();
+            },
+            trigger: ['change', 'blur'],
+          }
+        ],
+        password: [
+          { required: true, message: 'Please enter password', trigger: ['change', 'blur'] },
+          {
+            validator(rule, value, callback) {
+              console.log('validator rule, value, callback password');
+
+              if(value.length < 5) {
+                return callback('Password is too weak')
+              }
+
+              return callback();
+            }
+          }
+        ],
+        sick: [
+          { required: true, message: 'Please choose sick', trigger: ['change'] },
+          {
+            validator(rule, value, callback) {
+              if(value === 'Yes I am') {
+                return callback('Oh no !!!')
+              }
+
+              return callback();
+            }
+          }
+        ]
+      },
+      loginForm: {}
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$refs.formRef.validate((valid, fields) => {
+        if(!valid) {
+          return console.log('Error', fields);
+        }
+
+        this.loginForm = this.form;
+        return console.log('fields', this.form);
+      })
+    },
+    handleChangeRadio(e) {
+      console.log('handleChangeRadio', e);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+* {
+  font-family: 'Roboto', sans-serif;
+}
+
+.wrapper {
+  width: 50%;
+  margin: auto;
 }
 </style>
---
+++ tests/unit/example.spec.js
@@ -0,0 +1,169 @@
+import { expect } from 'chai'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { FormItem, Input, Button, Form, Radio, RadioGroup } from 'element-ui'
+import App from '../../src/App.vue';
+
+let wrapper;
+const localVue = createLocalVue();
+localVue.use(FormItem)
+localVue.use(Input)
+localVue.use(Button)
+localVue.use(Form)
+localVue.use(Radio);
+localVue.use(RadioGroup);
+
+const mountConfig = () => {
+  return {
+    localVue,
+    stubs: {
+      ElForm: Form,
+      ElFormItem: FormItem,
+      ElInput: Input,
+      ElButton: Button,
+      ElRadioGroup: RadioGroup,
+      ElRadio: Radio,
+    }
+  }
+}
+
+describe('App.vue', () => {
+  afterEach(() => {
+    wrapper?.destroy();
+  })
+
+  it('should show the error alert when user not input username', async () => {
+    wrapper = shallowMount(App, mountConfig());
+
+    const el = wrapper.findComponent({ name: 'ElInput' });
+    el.element.value = ""
+    el.vm.$emit('input', null);
+    el.vm.$emit('blur');
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+    
+    expect(wrapper.vm.form.username).to.equal(null);
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Please enter username');
+  })
+
+  it('should show the error alert when user not input password', async () => {
+    const username = null; 
+
+    wrapper = shallowMount(App, mountConfig());
+
+    const els = wrapper.findAllComponents({ name: 'ElInput' });
+    const el = els.at(1);
+
+    // el.element.value = username;
+    el.vm.$emit('input', username);
+    el.vm.$emit('blur');
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+    
+    expect(wrapper.vm.form.password).to.equal(null);
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Please enter password');
+  })
+
+  it('should not show the error alert when user input username', async () => {
+    const username = "my username";
+
+    wrapper = shallowMount(App, mountConfig());
+
+    const el = wrapper.findComponent({ name: 'ElInput' });
+    // el.element.value = username;
+    el.vm.$emit('input', username);
+    el.vm.$emit('blur');
+    wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+
+    expect(wrapper.vm.form.username).to.be.equal(username);
+    expect(elErrorAlert.exists()).to.be.false;
+  })
+
+  it('should not show the error alert when user input password', () => {
+    const password = "my passwrod";
+
+    wrapper = shallowMount(App, mountConfig());
+
+    const el = wrapper.findAllComponents({ name: 'ElInput' }).at(1);
+    // el.element.value = password;
+    el.vm.$emit('input', password);
+    el.vm.$emit('blur');
+    wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+
+    expect(wrapper.vm.form.password).to.be.equal(password);
+    expect(elErrorAlert.exists()).to.be.false;
+  })
+
+  it('should show error when username is "leminhcuong"', async () => {
+    const username = "leminhcuong";
+
+    wrapper = shallowMount(App, mountConfig());
+    
+    const el = wrapper.findComponent({ name: 'ElInput' });
+    // el.element.value = username;
+    el.vm.$emit('input', username);
+    el.vm.$emit('blur');
+
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+
+    expect(wrapper.vm.form.username).to.equal(username);
+    expect(elErrorAlert.exists()).to.be.true;
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Please enter another username');
+  })
+
+  it('should show error when username is "leminhcuong"', async () => {
+    const username = "leminhcuong";
+
+    wrapper = shallowMount(App, mountConfig());
+    
+    const el = wrapper.findComponent({ name: 'ElInput' });
+    // el.element.value = username;
+    el.vm.$emit('input', username);
+    el.vm.$emit('blur');
+
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+
+    expect(wrapper.vm.form.username).to.equal(username);
+    expect(elErrorAlert.exists()).to.be.true;
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Please enter another username');
+  })
+
+  it('should show error when sick was not choosen', async () => {
+    // const value = ['No I am not', 'Yes I am', 'I do not remember']
+    
+    wrapper = shallowMount(App, mountConfig());
+
+    const el = wrapper.findComponent({ name: 'ElRadioGroup' });
+    el.vm.$emit('input', null);
+
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+    expect(elErrorAlert.exists()).to.be.true
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Please choose sick');
+  })
+
+  it('should show error when sick was choosen "Yes I am" as a value', async () => {
+    const value = ['No I am not', 'Yes I am', 'I do not remember']
+    
+    wrapper = shallowMount(App, mountConfig());
+
+    const el = wrapper.findComponent({ name: 'ElRadioGroup' });
+    el.vm.$emit('input', value[1]);
+
+    await wrapper.vm.$nextTick();
+
+    const elErrorAlert = wrapper.find('.el-form-item__error');
+    expect(elErrorAlert.exists()).to.be.true
+    expect(elErrorAlert.element.innerHTML.trim()).to.equal('Oh no !!!');
+  })
+})
